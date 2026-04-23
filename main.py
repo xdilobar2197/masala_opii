1d1ecd86853cee3ff5a1abe453f933f8b4727f40
Dilobar Xudayarova <xdilobar2197@gmail.com>
2026-04-23 16:43:34 +0500

    Update and rename README.md to main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+# masala_opii
+
+
+#1-misol
+
+#
+# class Student:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#     def introduce(self):
+#         print(f"Mening ismim{self.name}, yoshim{self.age}")
+#
+# student1   = Student("Ali", 20)
+# student1.introduce()
+
+# 2-misol
+
+# class Car:
+#     def __init__(self ,model, color):
+#         self.model = model
+#         self.color = color
+#
+#     def info(self):
+#         print(f"Bu {self.model}, rangi {self.color}")
+#
+# m1 = Car("cobalt",  "oq")
+# m1.info()
+
+# 3-misol
+
+
+# class Phone:
+#     def __init__(self, brend, price):
+#         self.brend = brend
+#         self.price  =price
+#
+#     def about(self):
+#         print(f"{self.brend} narxi {self.price}")
+# p1  =Phone('IPhone', '1200$')
